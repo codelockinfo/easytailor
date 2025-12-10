427ca53ab8d7cc630d2c9ee075bcabacecebea2c
Codelock <codelock2021@gmail.com>
2025-12-10 12:53:42 +0530

    Remove hover

--- privacy-policy.php
+++ privacy-policy.php
@@ -34,7 +34,7 @@ $seoOptions = [
     echo GA4Helper::generateBaseCode();
     ?>
     
-    <link href="assets/css/style1.css" rel="stylesheet">
+    <link href="assets/css/style2.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
--- tailors.php
+++ tailors.php
@@ -88,7 +88,7 @@ $seoOptions = [
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-    <link href="assets/css/style1.css" rel="stylesheet">
+    <link href="assets/css/style2.css" rel="stylesheet">
     
     <style>
         * {
